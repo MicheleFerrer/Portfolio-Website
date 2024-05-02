78767aeb0349cab674891fba5859b56f7c70f9b1
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-05-02 16:45:17 -0400

    fixed drop down menu issue (spelling typo on JavaScript) and began to add qualification data to html.

--- Aessets/js/main.js
+++ Aessets/js/main.js
@@ -32,14 +32,14 @@ navLink.forEach(n => n.addEventListener('click', linkAction)
 )
 
 /*==================== ACCORDION SKILLS ====================*/
-const skilsContent = document.getElementsByClassName('skills__content'),
+const skillsContent = document.getElementsByClassName('skills__content'),
     skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
     let itemClass = this.parentNode.className
 
     for (i = 0; i < skillsContent.length; i++) {
-        skilsContent[i].className = 'skills__content skills__close'
+        skillsContent[i].className = 'skills__content skills__close'
     }
     if (itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open'
